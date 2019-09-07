286fde9938d2a01e3b3fd5e8cad69ca56003d718
Maxwell Patterson <maxwellpatterson@dhcp-10-101-186-17.wireless.northwestern.private>
2019-09-07 10:29:10 -0500

    Added githelp.py for git instructions

---
+++ git_help.py
@@ -0,0 +1,10 @@
+
+
+# git status
+    #checks difference between local repo and github repo
+# git clone 
+    #matches what is in github
+# git add .
+    #gathers files for build (. indicates everything below current folder)
+# git commit -m "" (need message during commmit)
+# git push origin master (pushes files to github)
