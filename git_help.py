a7c3619649eb2e7043379942fb2cc98a75ba0b2d
Maxwell Patterson <maxwellpatterson@dhcp-10-101-147-123.wireless.northwestern.private>
2019-12-02 21:12:46 -0600

    added files

--- git_help.py
+++ git_help.py
@@ -5,6 +5,16 @@
 # git clone 
     #matches what is in github
 # git add .
-    #gathers files for build (. indicates everything below current folder)
-# git commit -m "" (need message during commmit)
+    #gathers files for build 
+    # The '.' indicates everything below current folder)
+# git commit -m "" 
+    # (need message during commmit)
 # git push origin master (pushes files to github)
+
+# git reset --hard origin master
+
+#Add Branch
+#git checkout -b "branch name"
+
+#Push from local to branch
+# git push --set-upstream origin branch_name
